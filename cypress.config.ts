cd4e759f454a6c44a39d25ad1e1aa45514d62a1f
mgrybel <grybel.m.m@gmail.com>
2024-05-16 20:20:05 +0200

    Updated cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'cypress';
 import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
 import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
-import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild';
+import createEsbuildPlugin from '@badeball/cypress-cucumber-preprocessor/esbuild';
 
 export default defineConfig({
   e2e: {
     specPattern: '**/*.feature',
-    testIsolation: false,
     async setupNodeEvents(
       on: Cypress.PluginEvents,
       config: Cypress.PluginConfigOptions
